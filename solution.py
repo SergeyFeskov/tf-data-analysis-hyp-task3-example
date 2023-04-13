040ab6f8be4d21160c8035b293bf5b78bfa11bb0
SergeyFeskov <90043782+SergeyFeskov@users.noreply.github.com>
2023-04-13 23:27:02 +0300

    baseline

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 875744266  # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control_sample, test_sample) -> bool:
+    return (
+        ttest_ind(
+            test_sample, control_sample, alternative="less", equal_var=True
+        ).pvalue
+        < 0.01
+    )
